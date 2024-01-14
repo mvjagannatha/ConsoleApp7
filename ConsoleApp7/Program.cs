5640427dc07b9261a7002e01f5cab09f0baf4a7f
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 13:52:50 +0530

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace prime
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for(i=2;i<=100;i++)
+            {
+                for (j = 2; j <= 100; j++)
+                    if (i % j == 0)
+                        break;
+                if(j>(i/j))
+                Console.WriteLine("{0} is prime", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
